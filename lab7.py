466ade74b6d836e0b54f766723a8048511674d5e
Cankat Acarer <cankatacarer@gmail.com>
2023-05-09 11:06:21 +0300

    Initial commit

---
+++ lab7.py
@@ -0,0 +1,14 @@
+import cv2
+
+image = cv2.imread('/Users/cankatacarer/Desktop/nature.jpg')
+blue_channel, green_channel, red_channel = cv2.split(image)
+cv2.imshow('Blue Channel', blue_channel)
+cv2.imshow('Green Channel', green_channel)
+cv2.imshow('Red Channel', red_channel)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+green_channel = green_channel * 0
+modified_image = cv2.merge([blue_channel, green_channel, red_channel])
+cv2.imshow('Modified Image', modified_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
